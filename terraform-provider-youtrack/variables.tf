065bcc13cd5a59e2b4ed718c991a092bc968727c
elinevich <106667263+elinevich@users.noreply.github.com>
2023-05-27 14:37:16 +0300

    Delete variables.tf

--- terraform-provider-youtrack/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "youtrack_token" {
-  type    = string
-  default = "perm:cm9vdA==.NjMtMTM=.bRNGvO7SEn9ADdFsFvEFAYmc2Q2PPx"
-}
-
-variable "youtrack_base_url" {
-  type    = string
-  default = "dash.myjetbrains.com/api"
-}
